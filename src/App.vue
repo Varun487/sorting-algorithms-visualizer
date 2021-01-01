1789de5f2ea4021c1f909a9db0ee96acda72a9ea
Varun487 <seshuvarun@gmail.com>
2021-01-01 16:04:28 +0530

    Changed title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Sorting Algorithms Visualizer</h1>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Varun's Vue.js App"/>
 </template>
 
 <script>
