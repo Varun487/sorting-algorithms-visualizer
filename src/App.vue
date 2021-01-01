c7bc982c380c4575f1871210f45e05a9c4523f36
Varun487 <seshuvarun@gmail.com>
2021-01-01 16:18:26 +0530

    Changed title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Sorting Algorithms Visualizer</h1>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Shreya's Vue.js App"/>
+  <HelloWorld msg="Welcome to Algo Vue.js App"/>
 </template>
 
 <script>
